d6f4bb98cf27e1751c72206927651660d61f1d02
mdama <mcafeesociety@gmail.com>
2020-08-26 22:50:02 -0300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Header from "./components/Header";
-
+import Main from "./components/Main";
 import "./app.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Main>
+        <input type="text" /> <button>Adicionar</button>
+      </Main>
     </>
   );
 }
