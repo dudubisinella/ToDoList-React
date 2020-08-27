a6031fb0d2527abb6e63ab5d10d90915f74569c1
Eduardo Bisinella <bisinella7@gmail.com>
2020-08-26 22:54:23 -0300

    footer e main adc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <Main title="Projects" />
+
+      <Footer title="Projects" />
     </>
   );
 }
