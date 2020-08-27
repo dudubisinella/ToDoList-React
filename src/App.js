0cb593e342f219f939ab28de7c138ade589cd804
Eduardo Bisinella <bisinella7@gmail.com>
2020-08-26 22:59:59 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import "./app.css";
+import "./App.css";
 
 function App() {
   return (
