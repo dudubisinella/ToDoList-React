cbdffbce616ee1930fe22c02e51a1d6c0a14390e
Eduardo Bisinella <bisinella7@gmail.com>
2020-08-28 08:50:38 -0300

    simples lógica

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <Header title="To do List com React">
-        <div>
-          <ul></ul>
-        </div>
-      </Header>
+      <Header title="To do List com React" />
 
-      <Main>
-        <input type="text" placeholder="Digite Aqui" />
-        <button>Adicionar</button>
-      </Main>
+
+      <Main />
+
 
       <Footer title="Feito por Dama e Eduardo" />
     </>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Main({ title, children }) {
-  return (
-    <main>
-      {children}
-    </main>
-  );
+function Main() {
+
+    const [Lista, setLista] = useState([]);
+    const [novaTarefa, setnovaTarefa] = useState("");
+    const [owner, setOwner] = useState("");
+
+    function addNovaLista() {
+        const listas = { novaTarefa, owner, Id: `${Date.now()}` };
+
+        setLista([...Lista, listas]);
+        setnovaTarefa("");
+        setOwner("");
+    }
+
+    return (
+        <>
+            <input value={novaTarefa} placeholder="Digite aqui!" type="text" onChange={(e) => setnovaTarefa(e.target.value)} />
+            <br />
+            <input value={owner} placeholder="Digite aqui!" type="text" onChange={(e) => setOwner(e.target.value)} />
+            <br />
+            <button type="button" onClick={addNovaLista}>
+                Adicionar!
+            </button>
+            <br />
+
+            <h3>{novaTarefa}</h3>
+            <h3>{owner}</h3>
+
+            <ul>
+                {Lista.map((eachLista) => (
+
+                    <li key={eachLista.simpleId}>
+                        <b>{eachLista.owner}: </b>{eachLista.novaTarefa}.
+                    </li>
+
+                ))}
+            </ul>
+        </>
+    )
 }
+
+export default Main;
