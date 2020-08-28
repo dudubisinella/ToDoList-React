6c4e5984b48d479fa25a97f098a70db726aaea89
mdama <mcafeesociety@gmail.com>
2020-08-28 20:47:21 -0300

    leve estilização

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,44 +1,59 @@
 import React, { useState } from "react";
 
 function Main() {
+  const [Lista, setLista] = useState([]);
+  const [novaTarefa, setnovaTarefa] = useState("");
+  const [owner, setOwner] = useState("");
 
-    const [Lista, setLista] = useState([]);
-    const [novaTarefa, setnovaTarefa] = useState("");
-    const [owner, setOwner] = useState("");
+  function addNovaLista() {
+    const listas = { novaTarefa, owner, Id: `${Date.now()}` };
 
-    function addNovaLista() {
-        const listas = { novaTarefa, owner, Id: `${Date.now()}` };
+    setLista([...Lista, listas]);
+    setnovaTarefa("");
+    setOwner("");
+    // console.log(Lista);
+  }
 
-        setLista([...Lista, listas]);
-        setnovaTarefa("");
-        setOwner("");
-    }
+  return (
+    <>
+      <main>
+        <input
+          className="input"
+          value={owner}
+          placeholder="Digite aqui!"
+          type="text"
+          onChange={(e) => setOwner(e.target.value)}
+        />
+        <br />
+        <input
+          className="input"
+          value={novaTarefa}
+          placeholder="Digite aqui!"
+          type="text"
+          onChange={(e) => setnovaTarefa(e.target.value)}
+        />
+        <br />
+        <button type="button" onClick={addNovaLista}>
+          Adicionar!
+        </button>
+        <br />
 
-    return (
-        <>
-            <input value={novaTarefa} placeholder="Digite aqui!" type="text" onChange={(e) => setnovaTarefa(e.target.value)} />
-            <br />
-            <input value={owner} placeholder="Digite aqui!" type="text" onChange={(e) => setOwner(e.target.value)} />
-            <br />
-            <button type="button" onClick={addNovaLista}>
-                Adicionar!
-            </button>
-            <br />
+        <h2>{owner}</h2>
+        <h3>{novaTarefa}</h3>
 
-            <h3>{novaTarefa}</h3>
-            <h3>{owner}</h3>
-
-            <ul>
-                {Lista.map((eachLista) => (
-
-                    <li key={eachLista.simpleId}>
-                        <b>{eachLista.owner}: </b>{eachLista.novaTarefa}.
-                    </li>
-
-                ))}
-            </ul>
-        </>
-    )
+        <ul>
+          {Lista.map((eachLista) => (
+            <li key={eachLista.simpleId}>
+              <h2>
+                <b>{eachLista.owner}: </b>
+                {eachLista.novaTarefa}.
+              </h2>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
 
 export default Main;
