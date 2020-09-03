f2e27ae29308f5a9a5747d04205067eb511434f6
mdama <mcafeesociety@gmail.com>
2020-09-02 21:22:16 -0300

    crashando a tela

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Li from "./Li";
+// import Li from "./Li";
 
 function Main() {
   const [Lista, setLista] = useState([]);
@@ -13,6 +13,22 @@ function Main() {
     setnovaTarefa("");
     setOwner("");
     // console.log(Lista);
+  }
+
+  function Delete(id) {
+    // console.log(Lista.Id);
+    // //copy list
+    setLista(
+      ...Lista,
+      Lista.filter((lista) => lista.id !== id)
+    );
+    // setProjects(projects.filter((project) => project.id !== projectId));
+    // //filter out deleted object
+    // //remember that the list (each object) gets assigned a value and id when its added to list
+    // const updatedList = list.filter((item) => item.id !== id);
+
+    // //update state
+    // this.setState({ Lista: updatedList });
   }
 
   return (
@@ -44,11 +60,19 @@ function Main() {
 
         <ul>
           {Lista.map((eachLista) => (
-            <Li
-              id={eachLista.simpleId}
-              owner={eachLista.owner}
-              novaTarefa={eachLista.novaTarefa}
-            />
+            // <Li
+            //   id={eachLista.simpleId}
+            //   owner={eachLista.owner}
+            //   novaTarefa={eachLista.novaTarefa}
+            // />
+
+            <li key={eachLista.Id}>
+              <h2>
+                <b>{eachLista.owner}: </b>
+                {eachLista.novaTarefa}.
+              </h2>
+              <button onClick={() => Delete(Lista.id)}>Delete</button>
+            </li>
           ))}
         </ul>
       </main>
