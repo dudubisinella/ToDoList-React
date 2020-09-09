502f3930aca0be532f459bb8c7ac52f21a75dfae
mdama <mcafeesociety@gmail.com>
2020-09-09 19:35:13 -0300

    tentativas do Delete

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,19 +16,26 @@ function Main() {
   }
 
   function Delete(id) {
-    // console.log(Lista.Id);
-    // //copy list
     setLista(
-      ...Lista,
-      Lista.filter((lista) => lista.id !== id)
+      Lista.filter((lista) => lista.id !== id),
+      ...Lista
     );
-    // setProjects(projects.filter((project) => project.id !== projectId));
-    // //filter out deleted object
-    // //remember that the list (each object) gets assigned a value and id when its added to list
-    // const updatedList = list.filter((item) => item.id !== id);
+      // console.log(Lista.Id);
+      // //copy list
+      // setLista(
+      //   ...Lista,
+      //   Lista.filter((lista) => lista.id !== id)
+      // Lista.filter((lista) => lista.id !== id),
+      //   ...Lista
+      // );
+      // setProjects(projects.filter((project) => project.id !== projectId));
+      // //filter out deleted object
+      // //remember that the list (each object) gets assigned a value and id when its added to list
+      // const updatedList = list.filter((item) => item.id !== id);
 
-    // //update state
-    // this.setState({ Lista: updatedList });
+      // //update state
+      // this.setState({ Lista: updatedList });
+    }
   }
 
   return (
@@ -71,7 +78,7 @@ function Main() {
                 <b>{eachLista.owner}: </b>
                 {eachLista.novaTarefa}.
               </h2>
-              <button onClick={() => Delete(Lista.id)}>Delete</button>
+              <button onClick={() => Delete(eachLista.id)}>Delete</button>
             </li>
           ))}
         </ul>
